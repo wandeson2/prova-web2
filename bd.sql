bbf19f7e1a6c7417874c65e5d69426817590f24b
wandeson <wandeson36@gmail.com>
2018-06-14 23:41:42 -0300

    alterando banco

--- bd.sql
+++ bd.sql
@@ -1,38 +1,140 @@
-CREATE TABLE users(
-	id int UNSIGNED auto_increment ,
-	name varchar(30) NOT NULL,
-	username varchar(50) NOT NULL,
-	password varchar (500) NOT NULL,
-	email varchar(100) NOT NULL,
-	role VARCHAR(20) NOT NULL,
-	created DATETIME DEFAULT NULL,
-	modified DATETIME DEFAULT NULL,
-	PRIMARY KEY (id)
-);
+-- MySQL dump 10.13  Distrib 5.7.22, for Linux (x86_64)
+--
+-- Host: localhost    Database: prova
+-- ------------------------------------------------------
+-- Server version	5.7.22-0ubuntu0.16.04.1
 
-CREATE TABLE students(
-	id int UNSIGNED auto_increment ,
-	primary key(id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE monitors(
-	id int UNSIGNED auto_increment ,
-	name varchar(40) not null,
-	discipline varchar(30) not null,
-	primary key (id)
-);
+--
+-- Table structure for table `monitors`
+--
 
-CREATE TABLE students_monitors(
-	id int UNSIGNED auto_increment ,
-	student_id int UNSIGNED ,
-	monitor_id int UNSIGNED ,
-	date_time_start datetime not null,
-	date_time_fin datetime not null,
-	status enum('Realizado','Aluno Faltou','Cancelado','Pendente') not null,
-	feedback varchar(255) not null,
-	primary key(id),
-	foreign key (student_id) references students(id),
-	foreign key (monitor_id) references monitors (id)
-);
+DROP TABLE IF EXISTS `monitors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `monitors` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `discipline` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `monitors`
+--
 
+LOCK TABLES `monitors` WRITE;
+/*!40000 ALTER TABLE `monitors` DISABLE KEYS */;
+INSERT INTO `monitors` VALUES (1,'w','matematica');
+/*!40000 ALTER TABLE `monitors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `students` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (1,'wandeson'),(2,'w');
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `students_monitors`
+--
+
+DROP TABLE IF EXISTS `students_monitors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `students_monitors` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `student_id` int(10) unsigned DEFAULT NULL,
+  `monitor_id` int(10) unsigned DEFAULT NULL,
+  `date_time_start` datetime NOT NULL,
+  `date_time_fin` datetime NOT NULL,
+  `role` varchar(20) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `student_id` (`student_id`),
+  KEY `monitor_id` (`monitor_id`),
+  CONSTRAINT `students_monitors_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`),
+  CONSTRAINT `students_monitors_ibfk_2` FOREIGN KEY (`monitor_id`) REFERENCES `monitors` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students_monitors`
+--
+
+LOCK TABLES `students_monitors` WRITE;
+/*!40000 ALTER TABLE `students_monitors` DISABLE KEYS */;
+INSERT INTO `students_monitors` VALUES (1,1,1,'2018-06-14 16:56:00','2018-06-14 16:56:00','ccc','2018-06-14 16:56:34','2018-06-14 16:56:34'),(2,1,1,'2018-06-14 17:02:00','2018-06-14 17:02:00','AlunoFaltou','2018-06-14 17:02:48','2018-06-14 17:06:29'),(3,1,1,'2018-06-14 17:03:00','2018-06-14 17:03:00','Pendente','2018-06-14 17:03:18','2018-06-14 17:05:42'),(4,1,1,'2018-06-14 17:33:00','2018-06-14 17:33:00','Realizado','2018-06-14 17:33:20','2018-06-14 17:33:20');
+/*!40000 ALTER TABLE `students_monitors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `role` varchar(20) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'w','wa','$2y$10$kheWJ/Oo5PQk.x7U3TdWyOFlRX.Dy6Bblh.IsDSl4oJ.53mca1d5e','Wandeson36@gmail.com','monitors','2018-06-14 17:20:51','2018-06-14 17:20:51'),(2,'wandeson','w','$2y$10$M5TVKgSc0GWpDvEm6LU42uSGkPuzZ0HCNBg/OvphzhJvh.o/R9h/m','w@gmail.com','monitors','2018-06-15 01:08:34','2018-06-15 01:08:34'),(3,'jo','jo','$2y$10$Y9OLdshuyQlWdw3yD5DTie5Hkhqr9zCKN7SdxfVZHsg7FmHqwp4r2','ww@gmail.com','studants','2018-06-15 01:37:00','2018-06-15 01:37:00'),(4,'j','j','$2y$10$7V2acr5u/vUpwRMbsdzVPuT8KBs1T/lMj69cJ/ERr7bf/pglwIcMy','Wanden36@gmail.com','monitors','2018-06-15 01:40:41','2018-06-15 01:40:41');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-06-14 22:49:03
